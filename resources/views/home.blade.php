56819eb2d7ffaccbf9147c42026f442319d27489
Haqim <khaqim414@gmail.com>
2020-01-03 02:54:39 +0700

    install stisla template for admin page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('template_admin.home')
+@section('content')
+hello
+@endsection
---
+++ resources/views/template_admin/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+<div class="footer-left">
+  Copyright &copy; 2018 <div class="bullet"></div> Template By <a href="https://getstisla.com/getting-started">STISLA</a>
+</div>
+<div class="footer-right">
+  
+</div>
+</footer>
