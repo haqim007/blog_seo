40f422c7c7143ba7fa9396436082d55fbedbc77e
Haqim <khaqim414@gmail.com>
2020-01-05 01:52:56 +0700

    Add pagination for category list

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -1,22 +1,24 @@
 @extends('template_admin.home')
 @section('content')
 <h1>Ini adalah laman category</h1>
-@foreach($category as $result)
-	<table class="table table-hover table-sm">
-		<thead>
-			<tr>
-				<th>No</th>
-				<th>Nama Kategori</th>
-				<th>Action</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td></td>
-				<td> {{$result->name}} </td>
-				<td><a href="" class="btn btn-primary btn-sm">Edit</a><a href="" class="btn btn-danger btn-sm">Hapus</a></td>
-			</tr>
-		</tbody>
-	</table>
-@endforeach
+
+<table class="table table-hover table-sm table-bordered table-striped">
+	<thead>
+		<tr>
+			<th>No</th>
+			<th>Nama Kategori</th>
+			<th>Action</th>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach($category as $key => $result)
+		<tr>
+			<td>{{$key + $category->firstitem()}}</td>
+			<td> {{$result->name}} </td>
+			<td><a href="" class="btn btn-primary btn-sm">Edit</a> <a href="" class="btn btn-danger btn-sm">Hapus</a></td>
+		</tr>
+		@endforeach
+	</tbody>
+</table>
+{{$category->links()}}
 @endsection
